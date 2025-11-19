e31e191f0a6c5ba6941133978111f85db18b6c8d
wxn0brP <toja.filiper@gmail.com>
2025-11-19 08:48:33 +0100

    feat: update

--- src/core/startup/index.ts
+++ src/core/startup/index.ts
@@ -1,9 +1,6 @@
 import { delay } from "@wxn0brp/flanker-ui/utils";
 import "./style.scss";
 
-await new Promise(resolve => {
-    document.addEventListener("click", resolve, { once: true });
-});
 await delay(500);
 await import("./log");
 await import("./hi");

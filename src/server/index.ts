15c2a1cdde8c577058383f852f5e393088f9dd01
wxn0brP <toja.filiper@gmail.com>
2026-01-31 17:41:20 +0100

    chore: bump deps

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,11 @@
 import { FF_VQL } from "@wxn0brp/vql";
-import { app, oneWindow, port } from "@wxn0brp/zhiva-base-lib/server";
+import { app, oneWindow } from "@wxn0brp/zhiva-base-lib/server";
 import { VQL } from "./api";
 
 app.static("public");
 app.static("dist");
 
 FF_VQL(app, VQL);
-
-console.log(`Server listening on http://localhost:${port}`);
 
 if (process.env.NO_ZHIVA !== "true") {
     oneWindow();
